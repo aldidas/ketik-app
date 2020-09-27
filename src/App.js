57ab76d148f347bdbcdfc658a7c021742a4a2108
aldidas <aldi@daswanto.com>
2020-09-26 23:52:10 -0400

    initial setup code playground

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, useEffect } from 'react'
+import AceEditor from 'react-ace'
+import './App.css'
+import 'ace-builds/src-noconflict/mode-html'
+import 'ace-builds/src-noconflict/mode-javascript'
+import 'ace-builds/src-noconflict/mode-css'
+import 'ace-builds/src-noconflict/theme-gruvbox'
 
 function App() {
+  const iframeRef = useRef(null)
+  const [js, setJs] = useState('')
+  const [css, setCss] = useState('')
+  const [html, setHtml] = useState('')
+
+  useEffect(() => {
+    console.log(iframeRef.current.contentWindow.console)
+    //iframeRef.current.contentWindow.console.addEventListener('log', value => {
+    //console.log.apply(null, value)
+    //})
+  }, [])
+
+  const renderContent = () => {
+    const content = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Code Preview</title>
+            <style>${css}</style>
+        </head>
+        <body>
+           ${html} 
+           <script>${js}</script>
+        </body>
+        </html>
+        `
+    return `data:text/html,${encodeURIComponent(content)}`
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Header</h1>
       </header>
+      <main>
+        <section className="code">
+          <div>
+            <h2>HTML</h2>
+            <AceEditor
+              width="100%"
+              height="calc(47vh - 26px)"
+              mode="html"
+              theme="gruvbox"
+              onChange={setHtml}
+            />
+          </div>
+          <div>
+            <h2>Javascript</h2>
+            <AceEditor
+              width="100%"
+              height="calc(47vh - 26px)"
+              mode="javascript"
+              theme="gruvbox"
+              onChange={setJs}
+            />
+          </div>
+          <div>
+            <h2>CSS</h2>
+            <AceEditor
+              width="100%"
+              height="calc(47vh - 26px)"
+              mode="css"
+              theme="gruvbox"
+              onChange={setCss}
+            />
+          </div>
+        </section>
+        <section className="preview">
+          <iframe ref={iframeRef} title="preview" src={renderContent()} />
+          <div className="console">
+            <h2>Console</h2>
+            <div className="console-content">Content</div>
+          </div>
+        </section>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
